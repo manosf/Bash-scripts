459f60146ad7e7d1e874df8191c3553377a96b5e
Manos Fokas <31221999+manosf@users.noreply.github.com>
2017-09-07 23:47:45 +0300

    Added 'persistence' and 'help' options.

--- Screddit/screddit.sh
+++ Screddit/screddit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$EUID" != "0" ]; then
     echo "This script must be run as root" 1>&2
@@ -8,6 +8,9 @@ fi
 for args in $*
 do
     case $args in
+        --help|-h)
+            HELP="1";
+            ;;
         --filename|-f)
             FILENAME=$2;
             shift 2
@@ -16,9 +19,28 @@ do
             SUBREDDIT=$2;
             shift 2
             ;;
+        --persist|-p)
+            PERSIST="1";
+            shift
+            ;;
     esac
 done
 
+if [ "$HELP" == "1" ];
+then
+    printf "Options and arguments:\n"
+    echo "-f | --filename specifies a name for the wallpaper."
+    echo "-s | --subreddit specifies which subreddit to search."
+    echo "-p | --persist makes the wallpaper changes persist after rebooting."
+    exit 1
+fi
 
-wget -nc -O - "https://www.reddit.com/r/${SUBREDDIT}/top/?sort=top&t=week" | grep -oE 'https://i.imgur.com/[A-Za-z]+.jpg' | shuf -n 1 | xargs wget -O ${FILENAME}; 
+wget -nc -O - "https://www.reddit.com/r/${SUBREDDIT}/top/?sort=top&t=week" | grep -oE 'https://i.imgur.com/[a-zA-Z0-9]+.jpg' | shuf -n 1 | xargs wget -O ${FILENAME}; 
 feh --bg-scale ${FILENAME};
+
+if [ "$PERSIST" == "1" ];
+then
+    echo ~/.fehbg & >> ~/.xinitrc
+fi
+
+
