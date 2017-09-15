6a9082849a5db155ae0a434724fb443172716030
Manos Fokas <31221999+manosf@users.noreply.github.com>
2017-09-15 18:16:40 +0300

    Added multiple domains support.

--- Screddit/screddit.sh
+++ Screddit/screddit.sh
@@ -5,6 +5,7 @@ DIRECTORY="reddit_wallpapers"
 RESOLUTION="1920x1080"
 SUBREDDIT="spaceporn"
 JFILE="reddit_data.json"
+DOMAINLIST="i.redd.it i.imgur.com"
 
 if [ "$EUID" != "0" ]; then
     echo "This script must be run as root" 1>&2
@@ -57,11 +58,16 @@ curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0" -
 END=`jq '.data.children|length' ${JFILE}`
 for ((CHILD=0; CHILD<$END; CHILD++))
 do
-    if [[ `jq -r ".data.children[$CHILD].data.domain" ${JFILE}` == "i.redd.it" ]];
-    then
-        URL=`jq -r ".data.children[$CHILD].data.url" ${JFILE}`
-        wget -nc -P ${DIRECTORY} ${URL}
-    fi
+    DATA=`jq ".data.children[$CHILD]" ${JFILE}`
+    DOMAIN=`echo ${DATA} | jq -r '.data.domain'`
+    for DOM in ${DOMAINLIST}
+    do
+        if [[ "$DOM" == "$DOMAIN" ]];
+        then
+            URL=`echo ${DATA} | jq -r '.data.url'`
+            wget -nc -P ${DIRECTORY} ${URL}
+        fi
+    done
 done
 
 printf "Wallpapers downloaded and saved in ${DIRECTORY}.\n"
@@ -79,5 +85,5 @@ then
     DPATH=`realpath ${DIRECTORY} | rev | cut -d'/' -f2- | rev`    #Get the absolute path of $DIRECTORY and strip the $DIRECTORY from it
     OWNER=`ls -la ${DPATH}| sed -n 2p | awk '{print $3}'`         #Get the owner of $DIRECTORY's parent directory
     `chown -R ${OWNER} ${DPATH}`
-    printf "#!/bin/sh\nfeh --bg-scale --randomize --no-fehbg ${DPATH}/${DIRECTORY}/i.redd.it/*" > "/home/${OWNER}/.fehbg"
+    printf "#!/bin/sh\nfeh --bg-scale --randomize --no-fehbg ${DPATH}/${DIRECTORY}/*" > "/home/${OWNER}/.fehbg"
 fi
